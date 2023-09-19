4dd7b152a5547eb1d23dec11fa52c0f5fbb2e0f3
TehDucctator <100533074+TehDucctator@users.noreply.github.com>
2023-09-19 13:49:06 -0400

    Add assignment 1.5

---
+++ 1.5/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println("Enter a number with a decimal: ");
+    double num = s.nextDouble();
+    int numInt = (int)num;
+    
+    System.out.println("The biggest integer I know is " + Integer.MAX_VALUE);
+    System.out.println("If turn " + num + " into an integer (" + numInt + "), and add it to that, we get: "); 
+    System.out.println(Integer.MAX_VALUE + numInt);
+  }
+}
