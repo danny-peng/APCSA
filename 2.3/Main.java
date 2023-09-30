6a3259eed51bb31165aefd085d34f01e9c1ca7b5
TehDucctator <100533074+TehDucctator@users.noreply.github.com>
2023-09-30 19:24:33 -0400

    Add 2.3

---
+++ 2.3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Pet pet = new Pet("dog");
+
+        pet.getName();
+        pet.rename();
+        pet.getName();
+    }
+}
