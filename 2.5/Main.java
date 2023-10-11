f25a19fafa924e356d4421ce872f5118d15ceab6
TehDucctator <ducky543210@gmail.com>
2023-10-11 18:24:01 -0400

    Edit 2.5

--- 2.5/Main.java
+++ 2.5/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        double MP = 100;
+        
         Spell fireball = new Spell ("Fire", "Fireball");
 
         System.out.println("You gained a " + fireball.getElement() + " spell named " + fireball.getName());
         System.out.println("Damage: " + fireball.getDmg() + "\nSpeed: " + fireball.getSpeed() + "\nSize: " + fireball.getSize());
         System.out.println("Mana Cost: " + fireball.getManaCost() + " MP");
+
+        System.out.println("\nYou use " + fireball.getName());
+        MP = MP - fireball.getManaCost();
+        System.out.println("You now have " + MP + " MP left!");
     }
 }
