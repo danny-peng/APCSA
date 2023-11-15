acae46e8fb34c6de73b97fef0e4e24e06d10c74a
Danny <100533074+TehDucctator@users.noreply.github.com>
2023-11-15 13:49:41 -0500

    Add 3.7

---
+++ 3.7/Board.java
@@ -0,0 +1,9 @@
+public class Board {
+    private String moves = "";
+    // will add array to represent board once we learn arrays
+
+    public boolean equals(Board other) {
+        System.out.println("They are the same game!");
+        return moves.equals(other.moves);
+    }
+}
